6dd2676430918abcc0cac5419045b58e4db1c87b
Feggah <gabidferreira9@gmail.com>
2021-02-23 11:03:07 -0300

    Making poc same as django poc

--- app/main.py
+++ app/main.py
@@ -4,11 +4,6 @@ from random import randint
 
 app = FastAPI()
 
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
 @app.get("/number")
 async def generate_random_number():
     number = await random_number()
@@ -18,5 +13,5 @@ async def generate_random_number():
 
 async def random_number():
     # Doing some asynchronous stuff to generate the number
-    await sleep(60)
+    await sleep(10)
     return randint(1, 100)
